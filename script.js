abee6de617080a7a3d6953f818d8564e044dfbdb
“Marci-King” <marciomelin@msn.com>
2025-07-31 19:06:47 +0100

    Add getComputerChoice function with random selection

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-function add7(n) {
-  return n + 7;
+function getComputerChoice() {
+  let randomNumber = Math.random() * 3; // Generate random number between 0 and 2.999...
+
+  let choice = Math.floor(randomNumber);
+  // Convert to whole number (0, 1, or 2)
+
+  if (choice === 0) {
+    return "rock";
+  } else if (choice === 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
 
-console.log(add7(10));
-
-console.log("car");
-
-let car = "blue";
-
-car = car + "car";
-
-console.log(car);
+console.log(getComputerChoice());
